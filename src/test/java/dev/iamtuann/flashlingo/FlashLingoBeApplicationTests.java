6e8f20cc085c4a8d1b78cc4cf171941f0fe13ef2
nguyentuan <dungtuann67@gmail.com>
2024-10-16 08:36:11 +0700

    init project

---
+++ src/test/java/dev/iamtuann/flashlingo/FlashLingoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iamtuann.flashlingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashLingoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
